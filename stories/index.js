6afa5a02ac17245716a940f7bb852dbfc6e768d0
Margaryta <margaryta.lanova@gmail.com>
2022-12-02 12:26:44 -0800

    updated the component to use the props

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import Button from "components/Button";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -187,4 +188,9 @@ storiesOf("Appointment", module)
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")}
     />
-  ));
+  ))
+  .add("Status", () => (
+    <Status
+      message={"Deleting"}
+    />
+  ))
