08e6047ea27b3a83d20b29058dec788246858aab
Margaryta <margaryta.lanova@gmail.com>
2023-01-17 19:50:42 -0800

    created navigation.spec.js and added some tests

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,13 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.get("li")
+      .contains("Tuesday")
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+  })
+
+});
